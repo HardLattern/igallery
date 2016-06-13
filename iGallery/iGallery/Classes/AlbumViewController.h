a17619ba47854741f1d0ead6904ad134970fcd5f
Rathish Krish T <rathish.marthandan@zohocorp.com>
2016-06-13 12:03:53 +0530

    Added Xcode
    Added Xcode project files

---
+++ iGallery/iGallery/Classes/AlbumViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iGallery
+//
+//  Created by Rathish Krish T on 13/06/16.
+//  Copyright © 2016 Rathish Marthandan T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumViewController : UIViewController
+
+
+@end
+
---
+++ iGallery/iGallery/Classes/AlbumViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iGallery
+//
+//  Created by Rathish Krish T on 13/06/16.
+//  Copyright © 2016 Rathish Marthandan T. All rights reserved.
+//
+
+#import "AlbumViewController.h"
+
+@interface AlbumViewController ()
+
+@end
+
+@implementation AlbumViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
